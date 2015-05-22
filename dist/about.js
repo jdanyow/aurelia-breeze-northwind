b7012679a25c5db1939cca99ac72edfe0eb923bc
Jeremy Danyow <jdanyow@gmail.com>
2015-05-21 22:39:22 -0400

    bundled

---
+++ dist/about.js
@@ -0,0 +1,19 @@
+System.register([], function (_export) {
+  "use strict";
+
+  var About;
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  return {
+    setters: [],
+    execute: function () {
+      About = function About() {
+        _classCallCheck(this, About);
+      };
+
+      _export("About", About);
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFib3V0LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztNQUFhLEtBQUs7Ozs7Ozs7QUFBTCxXQUFLLFlBQUwsS0FBSzs4QkFBTCxLQUFLOzs7dUJBQUwsS0FBSyIsImZpbGUiOiJhYm91dC5qcyIsInNvdXJjZVJvb3QiOiIuLi9zcmMvIn0=
